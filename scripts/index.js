043021c0d0c67d5b68f84647eb016ad9fb50aed5
Jorge Proano <j.prortiz1@gmail.com>
2025-03-26 16:32:26 -0400

    Stage3

--- scripts/index.js
+++ scripts/index.js
@@ -31,3 +31,22 @@ const initialCards = [
 ];
 
 console.log(initialCards);
+
+const profileEditButton = document.querySelector(".profile__edit-button");
+
+const editModal = document.querySelector("#edit-modal");
+
+const editProfileModal = document.querySelector(".#edit-profile-modal");
+const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
+
+function openModal() {
+  editModal.classList.add("modal_opened");
+}
+
+function closeModal() {
+  editModal.classList.remove("modal_opened");
+}
+
+profileEditButton.addEventListener("click", openModal);
+
+editModalCloseBtn.addEventListener("click", closeModal);
